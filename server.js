41c51a39705347ddb73452c5b0a77f4019e758c0
Victor Ajagunna <victor-ajagunna@hotmail.com>
2023-03-20 14:04:32 +0100

    DashboardController completed

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ const MongoStore = require("mongodb");
 
 const app = express();
 
-app.use(session({
+app.use(
+  session({
     secret: "Jesus_is_Lord",
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create,
-    mongoUrl: process.env.MONGODB_URI
-}));
-//cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
-  // Date.now() - 30 * 24 * 60 * 60 * 1000
+    mongoUrl: process.env.MONGODB_URI,
+  })
+);
+//cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+// Date.now() - 30 * 24 * 60 * 60 * 1000
 // }));
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,24 +30,28 @@ app.use(express.json());
 
 // connectDB();
 
-
 //Static files
 app.use(express.static("public"));
 
 //Template Engine
-app.use(expressLayouts)
+app.use(expressLayouts);
 app.set("layout", "./Layouts/main");
 app.set("view engine", "ejs");
 
 //Route handlers
-app.get("/", () => { require("./server/routes/auth")});
-app.get("/", () => {require("./server/routes/index")});
-app.get("/", () => {require("./server/routes/dashboard")});
-
-app.get("*", () => {
-    res.status(404).render(404);
+app.get("/", () => {
+  require("./server/routes/auth");
+});
+app.get("/", () => {
+  require("./server/routes/index");
+});
+app.get("/", () => {
+  require("./server/routes/dashboard");
 });
 
+app.get("*", () => {
+  res.status(404).render(404);
+});
 
 const PORT = process.env.PORT || 6090;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
