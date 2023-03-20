12412979b6135d673ecd0e039470d75f5c3da398
Victor Ajagunna <victor-ajagunna@hotmail.com>
2023-03-20 12:35:43 +0100

    Dashboard routes done

--- server.js
+++ server.js
@@ -1,12 +1,52 @@
+require("dotenv").config();
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+connectDB = require("./server/config/db");
+const session = require("express-session");
+const passport = require("passport");
+const MongoStore = require("mongodb");
+
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: "Jesus_is_Lord",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create,
+    mongoUrl: process.env.MONGODB_URI
+}));
+//cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  // Date.now() - 30 * 24 * 60 * 60 * 1000
+// }));
 
-app.get("/", (req, res) => {	
-    res.send("Welcome to iBukun Notes");
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(methodOverride("_method"));
+
+// connectDB();
+
+
+//Static files
+app.use(express.static("public"));
+
+//Template Engine
+app.use(expressLayouts)
+app.set("layout", "./Layouts/main");
+app.set("view engine", "ejs");
+
+//Route handlers
+app.get("/", () => { require("./server/routes/auth")});
+app.get("/", () => {require("./server/routes/index")});
+app.get("/", () => {require("./server/routes/dashboard")});
+
+app.get("*", () => {
+    res.status(404).render(404);
 });
+
 
 const PORT = process.env.PORT || 6090;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
